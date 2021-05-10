20c6e959c63b484ef6342beab48288a8436ef188
haroldcampbell <harold.campbell@gmail.com>
2021-05-10 12:29:59 +0400

    Add reporting and recovery for panics

--- serverutils/actionstatus.go
+++ serverutils/actionstatus.go
@@ -2,8 +2,11 @@ package serverutils
 
 import (
 	"encoding/json"
+	"fmt"
 	"go_utils/utils"
 	"net/http"
+	"runtime"
+	"strings"
 )
 
 // ActionStatus return status for handlers
@@ -65,4 +68,41 @@ type ErrorResponse struct {
 func WriteErrorMessage(w http.ResponseWriter, errMessage string) {
 	b, _ := json.Marshal(ErrorResponse{DidFail: true, ErrorMessage: errMessage})
 	w.Write(b)
+}
+
+// https://gist.github.com/swdunlop/9629168
+func identifyPanic() string {
+	var name, file string
+	var line int
+	var pc [16]uintptr
+
+	n := runtime.Callers(3, pc[:])
+	for _, pc := range pc[:n] {
+		fn := runtime.FuncForPC(pc)
+		if fn == nil {
+			continue
+		}
+		file, line = fn.FileLine(pc)
+		name = fn.Name()
+		if !strings.HasPrefix(name, "runtime.") {
+			break
+		}
+	}
+
+	switch {
+	case name != "":
+		return fmt.Sprintf("%v:%v", name, line)
+	case file != "":
+		return fmt.Sprintf("%v:%v", file, line)
+	}
+
+	return fmt.Sprintf("pc:%x", pc)
+}
+
+func RecoverPanic(src string) {
+	r := recover()
+	if r == nil {
+		return
+	}
+	utils.Error(src, "PANIC:[%v] at %s", r, identifyPanic())
 }
